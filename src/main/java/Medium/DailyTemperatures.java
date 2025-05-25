ec2aec9fdb9fb8f5f87b9f06ab436d5ceffffc9e
leo <LevchenkoLM23@st.ithub.ru>
2025-05-25 19:53:31 +0300

    739

---
+++ src/main/java/Medium/DailyTemperatures.java
@@ -0,0 +1,20 @@
+package Medium;
+
+import java.util.Stack;
+
+public class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevDay = stack.pop();
+                result[prevDay] = i - prevDay;
+            }
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/Medium/DailyTemperaturesTest.java
@@ -0,0 +1,22 @@
+package Medium;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class DailyTemperaturesTest {
+    @ParameterizedTest
+    @MethodSource("testDailyTemperaturesProvide")
+    void testDailyTemperatures(int[] expected, int[] dailyTemperatures) {
+        Assertions.assertArrayEquals(expected, new DailyTemperatures().dailyTemperatures(dailyTemperatures));
+    }
+    static Stream<Object[]> testDailyTemperaturesProvide() {
+        return Stream.of(
+                new Object[]{new int[]{1,1,4,2,1,1,0,0}, new int[]{73,74,75,71,69,72,76,73}},
+                new Object[]{new int[]{1,1,1,0}, new int[]{30,40,50,60}},
+                new Object[]{new int[]{1,1,0}, new int[]{30,60,90}}
+        );
+    }
+}
